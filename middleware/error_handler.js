c6f2b140507c3a81f7870a69f2594ee42b5bb571
Wendy Wilgus <wilguswendy@gmail.com>
2023-03-08 13:09:12 -0500

    working on User model

--- middleware/error_handler.js
+++ middleware/error_handler.js
@@ -3,7 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err);
     const defaultError = {
-        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
         msg: err.message || 'Something went wrong. Try again later.',
     }
     if (err.name === 'ValidationError'){
@@ -12,6 +12,10 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         defaultError.msg = Object.values(err.errors)
             .map((item) => item.message)
             .join(',')
+    }
+    if(err.code && err.code === 11000) {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST;
+        defaultError.msg = `${Object.keys(err.keyValue)} must be unique`;
     }
     // res.status(defaultError.statusCode).json({ msg: err })
     res.status(defaultError.statusCode).json({ msg: defaultError.msg })
